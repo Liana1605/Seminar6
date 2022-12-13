fa19824819691a2ca51df9dcca6413b8b1a56d45
liana16 <liana_you@bk.ru>
2022-12-13 11:00:40 +0300

    Задача 39

---
+++ task39/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая перевернет одномерный массив 
+// (последний элемент будет на первом месте, а первый на последнем)
+//[1 2 3 4 5] -> [5 4 3 2 1]
+
+int[] arrOne = new int[6] {1,2,3,4,5,6};
+int[] arrTwo = new int[6];
+int iTwo = arrOne.Length - 1;
+
+for (int i = 0; i < arrOne.Length; i++)
+{
+    arrTwo[iTwo] = arrOne[i];
+    iTwo--;
+}
+foreach (int i in arrOne) Console.Write($" {i} ");
+Console.WriteLine();
+foreach (int i in arrTwo) Console.Write($" {i} ");
