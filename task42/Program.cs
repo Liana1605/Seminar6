2b6b4b583bc57c7e9a609d2f9822d635380e3be5
liana16 <liana_you@bk.ru>
2022-12-13 10:59:03 +0300

    Задача 42

---
+++ task42/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет преобразовывать десятисное число в двоичное
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+void ToBinarySystem(int Num)
+{
+    if(Num <= 0)
+    {
+        return;
+    }
+    ToBinarySystem(Num/2);
+    System.Console.Write(Num % 2);
+}
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+ToBinarySystem(number);
